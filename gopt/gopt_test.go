20d81690f230312878187fdc6f58cec411d53750
msmsny <msmsny@gmail.com>
2021-03-08 11:44:28 +0900

    add test code

---
+++ gopt/gopt_test.go
@@ -0,0 +1,61 @@
+package gopt
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommand(t *testing.T) {
+	testCases := map[string]struct {
+		name        string
+		options     string
+		packageName string
+		evaluate    string
+		fileName    string
+	}{
+		"basic": {
+			name:        "sample",
+			options:     "foo:string,bar:int,baz:bool",
+			packageName: "",
+			evaluate:    "true",
+			fileName:    "testdata/basic.go",
+		},
+		"withPackage": {
+			name:        "sample",
+			options:     "foo:string,bar:int,baz:bool",
+			packageName: "testdata",
+			evaluate:    "true",
+			fileName:    "testdata/withpackage.go",
+		},
+		"withoutEvaluate": {
+			name:        "sample",
+			options:     "foo:string,bar:int,baz:bool",
+			packageName: "testdata",
+			evaluate:    "false",
+			fileName:    "testdata/withoutevaluate.go",
+		},
+	}
+
+	for testCase, tt := range testCases {
+		t.Run(testCase, func(t *testing.T) {
+			cmd := NewGoptCommand()
+			destination := filepath.FromSlash(t.TempDir() + "/" + tt.fileName)
+			require.NoError(t, cmd.Flags().Set("name", tt.name))
+			require.NoError(t, cmd.Flags().Set("options", tt.options))
+			require.NoError(t, cmd.Flags().Set("package", tt.packageName))
+			require.NoError(t, cmd.Flags().Set("output", destination))
+			require.NoError(t, cmd.Flags().Set("evaluate", tt.evaluate))
+			require.NoError(t, cmd.Execute())
+
+			want, err := ioutil.ReadFile(tt.fileName)
+			require.NoError(t, err)
+			got, err := ioutil.ReadFile(destination)
+			require.NoError(t, err)
+			assert.Equal(t, string(want), string(got))
+		})
+	}
+}
---
+++ gopt/testdata/withoutevaluate.go
@@ -0,0 +1,27 @@
+package testdata
+
+type SampleOption interface{ apply(*sampleOptions) }
+
+type sampleOptions struct {
+	foo string
+	bar int
+	baz bool
+}
+
+type fooOption string
+
+func (o fooOption) apply(opts *sampleOptions) { opts.foo = string(o) }
+
+func WithFoo(value string) SampleOption { return fooOption(value) }
+
+type barOption int
+
+func (o barOption) apply(opts *sampleOptions) { opts.bar = int(o) }
+
+func WithBar(value int) SampleOption { return barOption(value) }
+
+type bazOption bool
+
+func (o bazOption) apply(opts *sampleOptions) { opts.baz = bool(o) }
+
+func WithBaz(value bool) SampleOption { return bazOption(value) }
