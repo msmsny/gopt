21d29f8bcc8b289db8fa946b26726643314750a5
msmsny <msmsny@gmail.com>
2021-03-16 00:39:07 +0900

    update test.yml, build test

--- test/cmd/buildtest/main.go
+++ test/cmd/buildtest/main.go
@@ -4,18 +4,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
 // This main command is a test code which creates test instance and prints.
 //
 // In order to build command, generate options
-// $ gopt --name sample --options foo:string,bar:int,baz:bool,qux:duration --package main -o test/cmd/buildtest/options.go
+// $ gopt --name sample --options 'foo:string,bar:int,baz:bool,qux:duration,quux:*net/http.Client' --package main -o test/cmd/buildtest/options.go
 //
 // Build test/cmd/buildtest and execute
 // $ go build -tags test ./test/cmd/buildtest
 // $ ./buildtest
-// &main.test{required:"required", foo:"foo", bar:1, baz:true, qux:1000000000}
+// &main.test{required:"required", foo:"foo", bar:1, baz:true, qux:1000000000, quux:(*http.Client)(nil)}, quux: &http.Client{Transport:http.RoundTripper(nil), CheckRedirect:(func(*http.Request, []*http.Request) error)(nil), Jar:http.CookieJar(nil), Timeout:0}
 func main() {
 	test := newTest(
 		"required",
@@ -23,8 +24,12 @@ func main() {
 		WithBar(1),
 		WithBaz(true),
 		WithQux(1000000000),
+		WithQuux(&http.Client{}),
 	)
-	fmt.Printf("%#v\n", test)
+	// dump quux directly for pointer address
+	quux := test.quux
+	test.quux = nil
+	fmt.Printf("%#v, quux: %#v\n", test, quux)
 }
 
 type test struct {
@@ -33,6 +38,7 @@ type test struct {
 	bar      int
 	baz      bool
 	qux      time.Duration
+	quux     *http.Client
 }
 
 func newTest(required string, options ...SampleOption) *test {
@@ -44,5 +50,6 @@ func newTest(required string, options ...SampleOption) *test {
 		bar:      opts.bar,
 		baz:      opts.baz,
 		qux:      opts.qux,
+		quux:     opts.quux,
 	}
 }
